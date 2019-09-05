5b48ad9eb42de79a111b405e34d0de46eddc419b
DCM <dcmay@ualberta.ca>
2019-09-05 12:01:13 -0600

    Less ramleaks (r)

--- Train_Eval.py
+++ Train_Eval.py
@@ -31,43 +31,51 @@ def __plot_training_curves(train_dict, val_dict):
                 plt.xlabel('number of Epochs')
                 plt.show()
 
+def __delete_list_slice(list, start, len_slice):
+    print('deleting from ', len(list), 'from ', start, 'to ', start+len_slice)
+    for step in range(len_slice):
+        del list[start]
+    return list
+
 def __slice_and_delete(inp, teacher, target, len_slice, seed):
-    num_total_samples = inp.shape[0]
-    len_slice = int(len_slice)
-    max_index_before_extraction = num_total_samples - len_slice
     np.random.seed(seed)
+
+    num_total_samples = len(inp)
+    len_slice = int(len_slice)
     index_start = np.random.uniform(low=0,
-                                       high=max_index_before_extraction,
+                                       high=num_total_samples - len_slice,
                                        size=None)
-    index_start = int(index_start)
-    test_slice = np.s_[index_start:(index_start + len_slice)]
-    inp_slice = inp[index_start:(index_start + len_slice), :, :]
-    inp = np.delete(inp, test_slice, axis=0)
-    teacher_slice = teacher[index_start:(index_start + len_slice), :, :]
-    teacher = np.delete(teacher, test_slice, axis=0)
-    slice_inputs = [inp_slice, teacher_slice, np.array([0] * inp_slice.shape[0])]
+    index_start = int(np.floor(index_start))
+    index_end = index_start + len_slice
 
-    target_slice = target[index_start:(index_start + len_slice), :, :]
-    target = np.delete(target, test_slice, axis=0)
+    inp_slice = inp[index_start:index_end][:][:]
+    inp = __delete_list_slice(inp, index_start, len_slice)
 
+    teacher_slice = teacher[index_start:index_end][:][:]
+    teacher = __delete_list_slice(teacher, index_start, len_slice)
 
+    slice_inputs = [np.array(inp_slice) , np.array(teacher_slice), np.array([0] * len(inp_slice))]
 
-    return slice_inputs, target_slice, inp, teacher, target
+    target_slice = target[index_start:index_end][:][:]
+    target = __delete_list_slice(target, index_start, len_slice)
 
-
+    return slice_inputs, np.array(target_slice), np.array(inp), np.array(teacher), np.array(target)
 
 def __split_dataset(inp, target, teacher, training_ratio):
     if training_ratio > 1:
         print('... seems like you want more than a full training set, the training ratio needs to be smaller than 1!')
+    inp = inp.tolist()
+    target = target.tolist()
+    teacher = teacher.tolist()
 
     remainder_for_test_val = 1.0-training_ratio
-    test_len =  (remainder_for_test_val/2.0) * inp.shape[0]
-    val_len = (remainder_for_test_val/2.0) * inp.shape[0]
+    test_len =  (remainder_for_test_val/2.0) * len(inp)
+    val_len = (remainder_for_test_val/2.0) * len(inp)
+
     dataset = {}
-
     dataset['test_inputs'], dataset['test_targets'], inp, teacher, target = __slice_and_delete(inp, teacher, target, test_len, seed=42)
-    dataset['val_inputs'], dataset['val_targets'], inp, teacher, target = __slice_and_delete(inp, teacher, target, val_len, seed=23)
-
+    print(len(inp))
+    dataset['val_inputs'], dataset['val_targets'], inp, teacher, target = __slice_and_delete(inp.tolist(), teacher.tolist(), target.tolist(), val_len, seed=23)
     dataset['train_inputs'] = [inp, teacher, np.array([1]*inp.shape[0])]
     dataset['train_targets'] = target
 
@@ -75,9 +83,44 @@ def __split_dataset(inp, target, teacher, training_ratio):
 
     return dataset
 
+def __build_model():
+    tf.keras.backend.clear_session()  # make sure we are working clean
+    from Building_Blocks import decoder_LSTM_block, block_LSTM
+    from Models import encoder_decoder_model, mimo_model
+    decoder_model=decoder_LSTM_block(num_layers=3,
+                                      num_units=64,
+                                      use_dropout=True,
+                                      dropout_rate=0.2,
+                                      use_attention=True,
+                                      attention_hidden=False)
+    encoder_block = block_LSTM(num_layers=3,
+                             num_units=64,
+                             use_dropout=True,
+                             dropout_rate=0.2,
+                             use_norm=True,
+                            only_last_layer_output=False)
+    # # ToDo: would it be smarter to have this in the encoder decoder thingie instead of outside?
+    projection_model=tf.keras.layers.Dense(units=out_shape[-1], activation=tf.keras.layers.Softmax(axis=-1))
 
+    model = encoder_decoder_model(encoder_block=encoder_block,
+                                  decoder_block=decoder_model,
+                                  projection_block=projection_model,
+                                  use_teacher=True,
+                                  input_shape=in_shape,
+                                  output_shape=out_shape,
+                                  )
 
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4) # set optimizers, metrics and loss
 
+    from Losses_and_Metrics import loss_wrapper
+    metrics = [loss_wrapper(last_output_dimension_size=out_shape[-1], loss_type='nME'),
+               loss_wrapper(last_output_dimension_size=out_shape[-1], loss_type='nMSE'),
+               loss_wrapper(last_output_dimension_size=out_shape[-1], loss_type='KL-D')]
+    model.compile(optimizer=optimizer,
+                  loss=loss_wrapper(last_output_dimension_size=out_shape[-1], loss_type='tile-to-forecast'),
+                  metrics=metrics) #compile, print summary
+    model.summary()
+    return model
 
 from Dataset_Loaders import get_Daniels_data, get_Lizas_data
 inp, ev_targets, ev_teacher, pdf_targets, pdf_teacher = get_Daniels_data()
@@ -89,68 +132,20 @@ in_shape = inp.shape[1:]
 
 dataset = __split_dataset(inp=inp, target=pdf_targets, teacher=pdf_teacher, training_ratio=0.6)
 
-tf.keras.backend.clear_session()  # make sure we are working clean
-from Building_Blocks import decoder_LSTM_block, block_LSTM
-from Models import encoder_decoder_model, mimo_model
-decoder_model=decoder_LSTM_block(num_layers=3,
-                                  num_units=128,
-                                  use_dropout=True,
-                                  dropout_rate=0.2,
-                                  use_attention=True,
-                                  attention_hidden=False)
-encoder_block = block_LSTM(num_layers=3,
-                         num_units=128,
-                         use_dropout=True,
-                         dropout_rate=0.2,
-                         use_norm=True,
-                        only_last_layer_output=False)
-# # ToDo: would it be smarter to have this in the encoder decoder thingie instead of outside?
-projection_model=tf.keras.layers.Dense(units=out_shape[-1], activation=tf.keras.layers.Softmax(axis=-1))
-
-model = encoder_decoder_model(encoder_block=encoder_block,
-                              decoder_block=decoder_model,
-                              projection_block=projection_model,
-                              use_teacher=True,
-                              input_shape=in_shape,
-                              output_shape=out_shape,
-                              )
-
-optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4) # set optimizers, metrics and loss
-
-from Losses_and_Metrics import loss_wrapper
-metrics = [loss_wrapper(last_output_dimension_size=out_shape[-1], loss_type='nME'),
-           loss_wrapper(last_output_dimension_size=out_shape[-1], loss_type='nMSE'),
-           loss_wrapper(last_output_dimension_size=out_shape[-1], loss_type='KL-D')]
-model.compile(optimizer=optimizer,
-              loss=loss_wrapper(last_output_dimension_size=out_shape[-1], loss_type='tile-to-forecast'),
-              metrics=metrics) #compile, print summary
-model.summary()
-
-
-
-hist_loss = []
-hist_val_loss = []
-hist_metric = []
-hist_val_metric = []
-
 relative_decrease = 0
 epoch = 0
+decrease = 0
+
 best_val_loss = np.inf
 prev_val_loss = np.inf
 best_val_metric = np.inf
 
-# ToDo: insert loop that adjusts the blend_factor from 1 to 0 depending on how the validation loss develops
-# ToDo: make this nicer, maybe with a isTrainig flag or sth?
 val_metrics = {}
 train_metrics = {}
-# inp_train = inp_train.tolist()
-# pdf_teacher_train = pdf_teacher_train.tolist()
-# pdf_targets_train = pdf_targets_train.tolist()
 
-# inp_val = inp_val.tolist()
-# pdf_teacher_val = pdf_teacher_val.tolist()
-# pdf_targets_val = pdf_targets_val.tolist()
-decrease = 0
+model = __build_model()
+
+
 while decrease < 10:
     train_history = model.fit(x=dataset['train_inputs'],
                               # train for a given set of epochs, look at history
